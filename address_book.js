11b5ae544dfcefaeb63f129d87cecab6de9f7abc
syed_umair98 <syed.umair.anwar@antronsys.com>
2020-12-15 09:45:45 +0530

    Added:created a address book

--- address_book.js
+++ address_book.js
@@ -1,1 +1,31 @@
-console.log("welcome to adress book")
+class AddessBook{
+    constructor(...param){
+        this.firstName=param[0]
+        this.lastName=param[1]
+        this.city=param[2]
+        this.state=param[3]
+        this.pincode=param[4]
+        this.phonenno=param[5]
+        this.email=param[6]
+    }set(firstName){
+        this._firstName=firstName
+    }
+    get(){return this._firstName}
+    set(lastName){this._lastName=lastName}
+    get(){return this._lastName}
+    set(city){this._city=city}
+    get (){return this._city}
+    set(state){this._state=state}
+    get(){return this._state}
+    set(pincode){this._pincode=pincode}
+    get (){return this._pincode}
+    set (phonenno){this._phonenno=phonenno}
+    get(){return this._phonenno}
+    set(email){this._email=email}
+    get(){return this._email}
+    toString(){
+        return "First Name "+this.firstName+" last name "+this.lastName+" city "+this.city+" state "+this.state+" pincode "+this.pincode+" phone no "+this.phonenno+" email "+this.email
+    }
+}
+let p1=new AddessBook("syed","umair","bhubneshwar","odisha",754221,8908922336,"umar101@gmail.com")
+console.log(p1.toString())
